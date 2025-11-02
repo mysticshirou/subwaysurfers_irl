3003f67b87e279b5fb4131af9e0428fdfada3cf3
HigherGround189 <thehigherground18@gmail.com>
2025-11-02 21:53:53 +0800

    commented out unused function

--- flask/app.py
+++ flask/app.py
@@ -39,17 +39,17 @@ def trigger_keyboard(action):
     return f'Action {action} sent!'
 
 # Generating mjpeg frames from webcam
-def generate_frames():
-    camera = cv2.VideoCapture(0)  # Camera source
-    while True:
-        success, frame = camera.read()
-        if not success:
-            break
-        else:
-            _, buffer = cv2.imencode('.jpg', frame)
-            frame_bytes = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+# def generate_frames():
+#     camera = cv2.VideoCapture(0)  # Camera source
+#     while True:
+#         success, frame = camera.read()
+#         if not success:
+#             break
+#         else:
+#             _, buffer = cv2.imencode('.jpg', frame)
+#             frame_bytes = buffer.tobytes()
+#             yield (b'--frame\r\n'
+#                    b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
 @app.route('/webcam-feed')
 def video_feed():
