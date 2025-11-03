7b232141e6bdc482208ca14271a683f9fbb435e7
HigherGround189 <thehigherground18@gmail.com>
2025-11-03 18:38:16 +0800

    changed back to camera feed url

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -11,8 +11,7 @@
     props: ["syncStatus"],
     data() {
       return {
-        // videoUrl: "http://127.0.0.1:5000/webcam-feed",
-        videoUrl: "https://images.unsplash.com/photo-1761872936204-07e2bbe1990b?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170",
+        videoUrl: "http://127.0.0.1:5000/webcam-feed",
       }
     },
     computed: {
